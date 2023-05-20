b5257e594c0179ce934234ee7bb832affd03327f
Lisabeth <106864761+PeppermintCoding123@users.noreply.github.com>
2023-05-20 08:26:13 +0200

    Update prog01_2.py

--- prog01_2.py
+++ prog01_2.py
@@ -9,45 +9,26 @@ f_derivative = lambda x: np.array([(x[0] + x[1]) * 2, (x[0] + x[1]) * 2 + 3 * np
 #%%
 #x0 = startvalue, H0 = initial Hessematrixapprox, a0 = initial stepwith, o = adaptive factor for a0, e = approx-closeness
 def BFGS(x0, H0, a0, o, e):
-    '''TODO: 
-        - Implement BFSG from Lecture
-        - use adaptive stepps for ak (can be resett on every step)
-    '''
-    """ Notes from Lecture:
-        sk = xk+1 - xk
-        yk = delta_f(xk+1) - delta_f(xk)
-        Hk+1 * yk = sk                      --- ist vertauschtes mit dem was an der Tafel stand? (5)
-        
-    strategy:
-        xk+1 = xk - Hk * f1_derivative
-    """
     # initialize iterations
     xk = x0
     Hk = H0
     fk = f(xk)
     fk_plus1 = np.inf
-    '''xk_plus1 = xk + ak*(-Hk)*f_derivative(xk)
-    fk = f(xk) # fk is always the f of the prefious xk
-    fk_plus1 = f(xk_plus1) # the f call on the current xk
-    '''
     res = []
     res.append(np.hstack((xk,fk)))
-    '''res.append(np.hstack((xk_plus1,fk_plus1)))
-    '''
+
     while np.linalg.norm(fk_plus1 - fk) > e:
         ak = a0
         fk = f(xk)
         # adjust ak to take step
         dfk = f_derivative(xk)
-        
-        #xk = xk_plus1
-        #fk = fk_plus1
-        xk_plus1 = xk + ak*(-Hk)*dfk
+
+        xk_plus1 = xk - ak*Hk.dot(dfk) #TODO: Merschweinchen: Is this the right function to create a new vector or should we usw something better?
         fk_plus1 = f(xk_plus1)
         
         while fk_plus1 >= fk:
                 ak *= o
-                xk_plus1 = xk + ak*(-Hk)*dfk
+                xk_plus1 = xk - ak*Hk.dot(dfk)
                 fk_plus1 = f(xk_plus1)
                 
         res.append(np.hstack((xk_plus1,fk_plus1)))
@@ -57,8 +38,8 @@ def BFGS(x0, H0, a0, o, e):
         yk = f_derivative(xk_plus1) - f_derivative(xk)
         rohk = 1/(np.dot(yk,sk))
         
-        rys = rohk * yk * sk.T
-        rsy = rohk * sk * yk.T
+        rys = rohk * np.outer(yk, sk.T)
+        rsy = rohk * np.outer(sk, yk.T) # rather use rys.T?
         Hk = Hk - (Hk * rys) - (rsy * Hk) + (rsy * Hk * rys) + (rohk * sk * sk.T)
         
         # move for next iteration
@@ -76,9 +57,53 @@ def BFGS(x0, H0, a0, o, e):
         - pathing with quiver
         - in 'Konturenplot' 
         - presision: e-6
-    Kommandozeile: 
-        - number of iterations = print(res.T[0].size - 1)
-        - x_sternchen (letztes xk_plus1)'''
+  '''
+#%% visualisation: 
+plt.close('all')
+
+# construct x- & y-Values
+x_values = np.linspace(-10, 10, 100)
+y_values = np.linspace(-10, 10, 100)
+x_grid, y_grid = np.meshgrid(x_values, y_values)
+
+# Define the starting points and functions
+x0 = np.array([-5, -5])
+a0 = 1;
+H0 = np.eye(2)
+o = 0.5
+e = 1e-2
+
+     
+# Calculate z according to x- & y-Values
+z_grid = np.array([[f(np.array([x, y])) for x in x_values] for y in y_values])
+
+# Drwa 3D-Surface
+fig = plt.figure()
+ax1 = fig.add_subplot(111, projection='3d')
+surf = ax1.plot_surface(x_grid, y_grid, z_grid, cmap=cm.YlOrRd, alpha=0.8)
+
+fig.colorbar(surf, shrink=0.5, aspect=5)
+        
+        
+
+#BFGS
+bfgs_points = BFGS(x0, H0, a0, o, e)
+drc = np.diff(bfgs_points, axis=0)
+ax1.quiver(bfgs_points[:-1, 0], bfgs_points[:-1, 1], bfgs_points[:-1, 2], drc[:, 0], drc[:, 1], drc[:, 2], color='blue', label='BFGS', scale = 1)
+# TODO: Merschweinchen: How to fix the issue with the puiver not functioning?      
+                
+# Labels
+plt.title(f'Figure BFGS with point x0 = {x0} in 3D' )
+ax1.set_xlabel('X-Achse')
+ax1.set_ylabel('Y-Achse')
+ax1.set_zlabel('Z-Achse')
+        
+ax1.legend()
+        
+plt.show()
+
+print('Total number of itterations to reach |xk - x*| < ' + str(e) + ' is: ' + str(np.shape(bfgs_points)))
+print('x* = ' + str(bfgs_points[-1]))
 
     
     
