1154f7b69e3de3bbe7beff4ebb29c784fa9c1dd3
Lisabeth <106864761+PeppermintCoding123@users.noreply.github.com>
2023-06-28 17:40:17 +0200

    Add files via upload

---
+++ prog03_1_01.py
@@ -0,0 +1,90 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.animation import FuncAnimation
+
+#%%
+def F_call (u, aufgabe):
+    if aufgabe == 'a':
+        A = np.array([[0, 1], [-1, 0]])
+        delta_u = A @ u
+        return delta_u
+    if aufgabe == 'b':
+        A = np.array([[0.5, -1],[1, -1]])
+        delta_u = A @ u
+        return delta_u
+    if aufgabe == 'c':
+        delta_u_up = np.sqt(u[1])
+        delta_u_down = -2 * u[0] * delta_u_up
+        delta_u = np.array([[delta_u_up],[delta_u_down]])
+        return delta_u
+    raise ValueError('Not known Aufgabe')
+
+def F_derivative(aufgabe):
+    if aufgabe == 'a':
+        A = np.array([[0, 1], [-1, 0]])
+        A_invers = np.linalg.solve(A, np.eye(2))
+        return A_invers
+    if aufgabe == 'b':
+        A = np.array([[0.5, -1],[1, -1]])
+        A_invers = np.linalg.solve(A, np.eye(2))
+        return A_invers
+    if aufgabe == 'c':
+        pass
+#%%
+def explisiteEuler(u0, tau, nbr_Steps, aufgabe):
+    res = []
+    u_tk = u0
+    
+    u_tk_x = u_tk[0]
+    
+    res.append(u_tk) # Step for tau = 0
+    
+    for t in range(1, nbr_Steps+1):
+        u_tkPlus1_x = u_tk_x + tau
+        F_tk = F_call(u_tk, aufgabe)
+        Ftk = F_tk * tau
+        u_tkPlus1 = (u_tk + Ftk)
+        u_tkPlus1_y = u_tkPlus1[1]
+        
+        # Prepare for next step 
+        u_tk_x = u_tkPlus1_x
+        u_tk = u_tkPlus1
+        res.append(np.hstack((u_tkPlus1_x, u_tkPlus1_y)))
+          
+    return np.array(res)
+
+#%%
+# choose exact exersise to display from ('a', 'b', 'c')
+chosen_aufgabe = 'a'
+#%% visualisation: 
+plt.close('all')
+
+if chosen_aufgabe == 'a':
+    u0 = np.array([0,1])
+    tau = 0.1
+    nbr_Steps = 100
+    
+    x = []
+    y = []
+    res = explisiteEuler(u0, tau, nbr_Steps, chosen_aufgabe)
+
+    figure, ax = plt.subplots()
+
+    ax.set_xlim(res[0,0],res[-1,0])
+    ax.set_ylim(-5,5)
+
+    line, = ax.plot(0,0)
+
+    def animation_function(i):
+        x.append(res[i,0])
+        y.append(res[i,1])
+        
+        line.set_xdata(x)
+        line.set_ydata(y)
+        return line,
+     
+    animation = FuncAnimation(figure, animation_function, frames = np.arange(0,100,1), interval = 70, repeat = False)
+    
+
+    plt.show()
